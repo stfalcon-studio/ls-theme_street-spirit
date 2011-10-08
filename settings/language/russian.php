0a9772aabdb321c2f196fc9415ac16d59ceaaeab
Shkodyak Andrey <fluxuator@gmail.com>
2011-10-08 14:41:54 +0300

    Исправлено отображение кнопок редактирования и удаления топиков для админа

--- settings/language/russian.php
+++ settings/language/russian.php
@@ -5,13 +5,32 @@
  * Содержит все текстовки движка.
  */
 return array(
-    // Common
+    'exit' => 'Выход',
+    'settings_menu_tuning' => 'Настройки сайта',
+    'menu_section' => 'Разделы',
     'search_keyword_text' => 'поиск по сайту',
+    // Blog
+    'blog_join_now' => 'Вступить в блог',
+    'blog_leave_now' => 'Покинуть блог',
+    'blog_menu_all_good' => 'Интересные',
+    'blog_menu_collective_good' => 'Интересные',
+    'blog_menu_collective_bad' => 'Не интересные',
+    'blog_menu_personal_good' => 'Интересные',
+    'blog_menu_personal_bad' => 'Не интересные',
+    // Comments
+    'comment_add' => 'отправить',
+    'comment_preview' => 'предварительный просмотр',
+    // Copyright
+    'text_skin_made_in' => 'Дизайн сделали в',
     // Topic
+    'topic_create' => 'Написать',
     'topic_rating' => 'рейтинг',
     'topic_vote_plus' => 'это интересно',
     'topic_vote_minus' => 'не интересно',
-    // Copyright
-    'text_skin_made_in' => 'Дизайн сделали в'
+    'topic_comment_add' => 'Прокомментировать',
+    // User
+    'user_authorization' => 'Войти',
+    'user_registration' => 'Зарегистрироваться',
+    'user_menu_profile_whois' => 'Пользователь',
 );
 ?>
